9933e8d1eae3378b04a90c32bacb6bf40a2b2947
Bao Mai <baomai@berkeley.edu>
2016-12-15 18:37:54 -0800

    All tests passed

--- test/factory-test.js
+++ test/factory-test.js
@@ -5,16 +5,21 @@ var should = require('chai').should();
 var Stub = require('../lib/stub');
 
 describe('Factory', function () {
+  var userStub = new Stub(function (options) {
+    return {
+      username: (options && options.username) || 'baoskee',
+      password: (options && options.password) || 'foobar'
+    }
+  }, 'users');
+  var tweetStub = new Stub(function () {
+    return {
+      content: 'Just setting up my twttr'
+    }
+  }, 'tweets');
   var factory = new Factory(MONGO_URI);
   var data = { username: 'something', password: 'lmfao' };
+
   beforeEach(function (done) {
-    var userStub = new Stub(function (options) {
-      return {
-        username: (options && options.username) || 'baoskee',
-        password: (options && options.password) || 'foobar'
-      }
-    }, 'users');
-    factory.add(100, userStub, data).add(200, userStub, data);
     done();
   });
 
@@ -26,6 +31,7 @@ describe('Factory', function () {
   });
 
   it('should populate current plan', function (done) {
+    factory.add(100, userStub, data).add(200, userStub, data);
     factory.exec(function (err, users) {
       if (err) throw err;
       users.length.should.equal(300);
@@ -49,5 +55,35 @@ describe('Factory', function () {
       });
     });
   });
+
+  describe('Testing cleanUp', function () {
+    beforeEach(function (done) {
+      factory.add(10, userStub).add(10, tweetStub).exec(function (err, docs) {
+        if (err) throw err;
+        docs.length.should.equal(20);
+        done();
+      });
+    });
+
+    it('cleanUp() should delete all docs from all collections', function (done) {
+      factory.cleanUp(function (err) {
+        if (err) throw err;
+
+        MongoClient.connect(MONGO_URI, function (err, db) {
+          db.collection('tweets').find({}).toArray(function (err, tweets) {
+            if (err) throw err;
+            tweets.length.should.equal(0);
+            db.collection('users').find({}).toArray(function (err, users) {
+              if (err) throw err;
+              users.length.should.equal(0);
+              done();
+            });
+          });
+        });
+      });
+    });
+
+  });
+
 });
 
